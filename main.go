6e8977c05dc2b8776101315ae6c3dead90a99c08
BW-Devil <weunknowing@gmail.com>
2023-04-19 23:01:01 +0800

    new function

--- main.go
+++ main.go
@@ -1,8 +1,45 @@
 package main
 
-import "LANShare/server"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
-	server := server.NewServer("192.168.3.79", 8888)
-	server.Start()
+	// server := server.NewServer("192.168.3.79", 8888)
+	// server.Start()
+	// port = 31539
+
+	var netInfo map[string]string = make(map[string]string)
+
+	ifaces, err := net.Interfaces()
+
+	if err != nil {
+		fmt.Println("获取网络接口信息失败", err)
+		os.Exit(1)
+	}
+
+	for _, iface := range ifaces {	
+		// 获取对应网络接口的IP
+		addrs, err := iface.Addrs()
+
+		if err != nil {
+			fmt.Printf("获取%s接口的IP地址失败\n", iface.Name)
+		}
+
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					netInfo[iface.Name] = ipnet.IP.String()
+				}
+			}
+		}
+	}
+
+	for name, ip := range netInfo {
+		fmt.Println(name)
+		fmt.Println(ip)
+	}
+
 }
